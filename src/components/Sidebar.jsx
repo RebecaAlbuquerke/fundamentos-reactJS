f50ca591a7bbe7e94773cb004f955c4e95418641
RebecaAlbuquerke <rebecaalbuquerke@gmail.com>
2024-04-08 14:17:40 -0300

    #F210

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from 'phosphor-react'
 
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return (
@@ -8,7 +9,7 @@ export function Sidebar() {
             <img className={styles.cover} src='https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/rebecaalbuquerke.png" />
+                <Avatar src="https://github.com/maykbrito.png" />
 
                 <strong>Rebeca Albuquerque</strong>
                 <span>Web dev</span>
