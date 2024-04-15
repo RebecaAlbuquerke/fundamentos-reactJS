00dfb40bdee07e9d956af4d32dec8523235595c7
RebecaAlbuquerke <rebecaalbuquerke@gmail.com>
2024-04-15 15:55:37 -0300

    #F217

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -39,6 +39,10 @@ export function Post({ author, publishedAt, content }) {
         setNewCommentText(event.target.value);
     }
 
+    function deleteComment(comment) {
+        console.log('Deletar comentário ${comment}')
+    }
+
     return(
         <article className={styles.post}>
             <header>
@@ -82,7 +86,7 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment key={comment} content={comment} />
+                    return <Comment key={comment} content={comment} onDeleteComment={deleteComment} />
                 })}
             </div>
 
